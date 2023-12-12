e7a7d6b7e6f81722dace03c5292c8becbaf5c83c
Artem Lisieienko <lisieienko.artem@gmail.com>
2023-12-12 16:38:24 +0200

    upg

--- client/src/components/Main/Degustation.vue
+++ client/src/components/Main/Degustation.vue
@@ -134,7 +134,7 @@ export default {
         const response = await axios({
           method: "post",
           withCredentials: false,
-          url: "https://cursova-prod.vercel.app:5000",
+          url: "https://cursova-prod.vercel.app",
           params: {
             name: this.name,
             address: this.address,
